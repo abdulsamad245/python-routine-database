3e48b4e8b5640da28194a26d9952c6b7d88f5162
devabdulsamad <100769627+abdulsamad245@users.noreply.github.com>
2025-11-23 16:02:02 +0100

    Update database_backend.py

--- database_backend.py
+++ database_backend.py
@@ -8,16 +8,12 @@ def connect():
     conn.commit()
     conn.close()
 
-
 def insert(date ,earnings ,exercise ,study ,diet ,python ):
     conn=sqlite3.connect('routine.db')
     cur=conn.cursor()
     cur.execute('INSERT INTO routine VALUES(NULL ,?,?,?,?,?,?)',(date ,earnings ,exercise ,study ,diet ,python ))
     conn.commit()
     conn.close()
-insert('1-2-2020','300','no-exercise','not studied','diet taken','did python') 
-#insert('1-3-2020','200','no-exercise','not studied','diet taken','did python') 
-
 
 def view():
     conn=sqlite3.connect('routine.db')
@@ -28,10 +24,6 @@ def view():
     conn.close()
     print((rows))
     return rows
-   
-
-#view()
-
 
 def search(date='' ,earnings='' ,exercise='' ,study='' ,diet='' ,python='' ):
     conn=sqlite3.connect('routine.db')
@@ -42,19 +34,13 @@ def search(date='' ,earnings='' ,exercise='' ,study='' ,diet='' ,python='' ):
     conn.close()
     return rows
 
-#print(search(400))
-    
-
-
 def delete(id):
     conn=sqlite3.connect('routine.db')
     cur=conn.cursor()
     cur.execute('DELETE FROM routine WHERE id=?',(id,))
     conn.commit()
     conn.close()
-
     
-#delete(4)
 connect()
     
 
